dff23f9d5553d96b40b37adc80f7776a8a7b71fd
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-11-05 15:19:35 -0800

    Fix syntax error in environment plugin

--- _plugins/environment_settings.rb
+++ _plugins/environment_settings.rb
@@ -4,10 +4,11 @@ module Jekyll
       site.config['cms'] = ENV['CMS_ENV'] || 'production'
       # Add other environment variables to `site.config` here...
       site.config['minify_html'] = ENV['JEKYLL_ENV'] == 'production'
-      site.config['srcset'] ||= {}
       if ENV['JEKYLL_ENV'] == 'production'
+        site.config['srcset'] ||= {}
         site.config['srcset']['optipng'] = true
         site.config['srcset']['cache'] = '/opt/build/cache/srcset'
+      end
     end
   end
 end
