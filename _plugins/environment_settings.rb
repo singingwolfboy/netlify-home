f97a328011fe874fed64eeb6994a9930c3fe3310
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-11-05 15:13:46 -0800

    implement cache for srcset

--- _plugins/environment_settings.rb
+++ _plugins/environment_settings.rb
@@ -3,7 +3,11 @@ module Jekyll
     def generate(site)
       site.config['cms'] = ENV['CMS_ENV'] || 'production'
       # Add other environment variables to `site.config` here...
-      site.config['minify_html'] = site.config['cms'] == 'production'
+      site.config['minify_html'] = ENV['JEKYLL_ENV'] == 'production'
+      site.config['srcset'] ||= {}
+      if ENV['JEKYLL_ENV'] == 'production'
+        site.config['srcset']['optipng'] = true
+        site.config['srcset']['cache'] = '/opt/build/cache/srcset'
     end
   end
 end
